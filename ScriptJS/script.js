dbde95fb4125e7f70e2452f7d962d35c133f7bd3
brualvess <oliveirabrunna2210@gmail.com>
2022-04-20 23:28:14 -0300

    layout desktop8

--- ScriptJS/script.js
+++ ScriptJS/script.js
@@ -26,3 +26,6 @@ function retornoApiQuizz(resposta){
 function buttomCriar(){
     window.location.replace("desktop8.html")
 } 
+function prosseguir(){
+    window.location.replace("desktop8.html")
+}
