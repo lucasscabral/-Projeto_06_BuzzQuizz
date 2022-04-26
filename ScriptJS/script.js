f62a3c7bf08a8940dd68e86d15930c365d4241c3
Lucas Cabral <lucas.9cabral41@gmail.com>
2022-04-26 01:45:21 -0300

    Finalização das respostas de um Quizz e adicionando a funcionalidade de reiniciar um quizz ou voltar para o home

--- ScriptJS/script.js
+++ ScriptJS/script.js
@@ -1,4 +1,6 @@
 let quizzesDaApi;
+let idQUizz;
+let totalAcertos = 0;
 
 quizzesApi();
 function quizzesApi() {
@@ -15,7 +17,7 @@ function retornoApiQuizz(resposta) {
   todasQuizzes.innerHTML = "";
 
   for (let i = 0; i < quizzesDaApi.length; i++) {
-    todasQuizzes.innerHTML += ` <div class="quizzes" onclick="entarQuizzApi(this)" id="${quizzesDaApi[i].id}">
+    todasQuizzes.innerHTML += ` <div class="quizzes" onclick="entrarQuizzApi(this)" id="${quizzesDaApi[i].id}">
                                         <div class="imagem-quizz">
                                             <img src="${quizzesDaApi[i].image}" alt="Quizzes">
                                             <span>${quizzesDaApi[i].title}</span>
@@ -28,15 +30,10 @@ function buttomCriar() {
   window.location.replace("desktop8.html");
 }
 
-function entarQuizzApi(elemento) {
-  console.log(elemento);
+function entrarQuizzApi(elemento) {
 
-  const conteudoQuizzes = document
-    .querySelector(".box-quizzes")
-    .classList.add("escondido");
-  const aparecerTelaQuizz = document
-    .querySelector(".box-questoes")
-    .classList.remove("escondido");
+  const conteudoQuizzes = document.querySelector(".box-quizzes").classList.add("escondido");
+  const aparecerTelaQuizz = document.querySelector(".box-questoes").classList.remove("escondido");
 
   const scrollarProTopo = document.querySelector("body");
   scrollarProTopo.scrollIntoView(true);
@@ -44,23 +41,17 @@ function entarQuizzApi(elemento) {
   const buscarUmQuizz = axios.get(
     `https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes/${elemento.id}`
   );
-  console.log(buscarUmQuizz);
   buscarUmQuizz.then(buscarIdQuizz);
 }
 
 let quizzEscolhido;
 
-
-
 function embaralharRespostas() { 
   return Math.random() - 0.5; 
 }
 
-
 function buscarIdQuizz(resposta) {
-  let numeroRespostas = [];
   quizzEscolhido = resposta.data;
-  console.log(quizzEscolhido);
 
   let questoesQuizzes = document.querySelector(".box-questoes");
   let questoesMontadas = "";
@@ -88,7 +79,6 @@ function buscarIdQuizz(resposta) {
           quizzEscolhido.questions[i].answers.sort(embaralharRespostas);
 
         for (let x = 0; x < quizzEscolhido.questions[i].answers.length; x++){
-          numeroRespostas.push(quizzEscolhido.questions[i].answers[x]);
           questoesMontadas += `
                                             <div class="box-respostas" data-correct="${quizzEscolhido.questions[i].answers[x].isCorrectAnswer}" onclick="selecionarResposta(this)">
                                                 <img src="${quizzEscolhido.questions[i].answers[x].image}" alt="">
@@ -107,13 +97,18 @@ function buscarIdQuizz(resposta) {
                               </div> 
                                   `;
   
-   questoesQuizzes.innerHTML = questoesMontadas;                    
+   questoesQuizzes.innerHTML = questoesMontadas;                  
 }
 
 function selecionarResposta(escolhida){
   escolhida.classList.add("escolhido");
 
   let todasRespostas = escolhida.parentNode.querySelectorAll('.box-respostas');
+  let respostaCorreta = escolhida.dataset.correct;
+
+  if(respostaCorreta == 'true'){
+    totalAcertos++;
+  }
 
   for(let i = 0; i<todasRespostas.length; i++){
     todasRespostas[i].classList.add("box-respostas-desabilitadas");
@@ -127,6 +122,7 @@ function selecionarResposta(escolhida){
   }  
   
   escolhida.classList.remove("box-respostas-desabilitadas");
+  
   proximaPergunta(escolhida);
 }
 
@@ -135,12 +131,82 @@ function proximaPergunta(escolhida){
   let proximaPergunta = perguntaAtual.nextElementSibling;
  
   if(proximaPergunta === null){
-    alert("Mostrar resultado");
+    setTimeout(mostrarResultado, 2000);
   }else{
     setTimeout(() => {
-      proximaPergunta.scrollIntoView(true);
+      proximaPergunta.scrollIntoView(
+        {
+          behavior: 'smooth'
+        }
+      );
     }, 2000);
-  
   }
+}
+
+/**
+ * funcao criada pois alguns levels de quizzes nao estavam cadastrados com o minValue na ordem crescente
+ * e essa funcao ajuda a comparar o valor de forma mais precisa e mostrar o resultado correto
+ */
+function ordenarPorValorLevel(a, b){
+  return a.minValue - b.minValue;
+}
+
+function mostrarResultado(){
+  let porcentagemDeAcertos = Math.round((totalAcertos * 100) / quizzEscolhido.questions.length) ;
+  let resultadoBox = document.querySelector(".box-questoes-quizzes");
+
+  let resultado = {};
+  let levels = [];
+
+  for(let i = 0; i<quizzEscolhido.levels.length; i++){
+    levels.push(quizzEscolhido.levels[i]);
+  }
+
+  levels.sort(ordenarPorValorLevel);
   
+  for(let i = 0; i<levels.length; i++){
+    if(porcentagemDeAcertos >= levels[i].minValue){
+        resultado = {
+          title: levels[i].title,
+          image: levels[i].image,
+          text: levels[i].text
+        };
+    }
+  }
+
+  resultadoBox.innerHTML +=`  <div class="box-resultado-quizz">
+                                        <div class="titulo-resultado">
+                                            <span>${porcentagemDeAcertos}% de acerto: ${resultado.title}</span>
+                                        </div>
+                                        <div class="conteudo-resultado">
+                                            <div class="box-resultado-img">
+                                                <img src="${resultado.image}" alt="">
+                                            </div>
+                                            <div class="box-descricao-resultado">
+                                                <span>${resultado.text}</span>
+                                            </div>
+                                        </div>
+                              </div>
+                              <div class="bnt-reiniciar-quizz" onclick="reiniciarJogo()">
+                                <span>Reiniciar Quizz</span>
+                              </div>
+                              <div class="bnt-voltar-home" onclick="voltarHome()">
+                                <span>Voltar pra home</span>
+                              </div>`;
+
+
+  let tabelaResultado = document.querySelector(".box-resultado-quizz");
+  tabelaResultado.scrollIntoView(
+    {
+      behavior: 'smooth'
+    })
+}
+
+function reiniciarJogo(){
+  totalAcertos = 0;
+  entrarQuizzApi(quizzEscolhido);
+}
+
+function voltarHome(){
+  window.location.reload();
 }
